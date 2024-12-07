58fc753ec990457d28000c54e93eea19a611d295
fixonz <109214021+fixonz@users.noreply.github.com>
2024-12-07 16:08:16 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Simli Agent",
-  description: "create-simli-agent",
+  title: "pMarca terminal",
+  description: "pmarca-terminal",
 };
 
 export default function RootLayout({
