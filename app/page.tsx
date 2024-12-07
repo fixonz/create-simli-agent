bc07262640132c3218c5c3c46942aa69e33d6f43
fixonz <109214021+fixonz@users.noreply.github.com>
2024-12-07 15:54:40 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,18 +22,15 @@ const Demo: React.FC = () => {
 
   return (
     <div className="bg-black min-h-screen flex flex-col items-center font-abc-repro font-normal text-sm text-white p-8">
-      <SimliHeaderLogo />
       <Navbar />
 
       <div className="absolute top-[32px] right-[32px]">
         <text
           onClick={() => {
-            window.open("https://github.com/simliai/create-simli-agent");
+            window.open("#");
           }}
           className="font-bold cursor-pointer mb-8 text-xl leading-8"
-        >
-          <Image className="w-[20px] inline mr-2" src={GitHubLogo} alt="" />
-          pmarca .... the real terminal
+        >          pmarca .... the real terminal
         </text>
       </div>
       <div className="flex flex-col items-center gap-6 bg-effect15White p-6 pb-[40px] rounded-xl w-full">
