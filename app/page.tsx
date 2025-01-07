4a411b3b6f852664595dd28bdd0d3398003ca1dc
fixonz <109214021+fixonz@users.noreply.github.com>
2025-01-07 08:32:28 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
- "use client";
+"use client";
 import React from "react";
 import SimliAgent from "@/app/SimliAgent";
 
@@ -12,9 +12,13 @@ const Demo: React.FC = () => {
   };
 
   return (
-    <div className="bg-black min-h-screen flex flex-col items-center justify-center font-abc-repro font-normal text-sm text-white p-8">
-      <div className="w-full max-w-[800px] aspect-video bg-effect15White p-6 rounded-xl">
-        <SimliAgent onStart={onStart} onClose={onClose} />
+    <div className="bg-black min-h-screen flex items-center justify-center font-abc-repro font-normal text-sm text-white p-4">
+      <div className="w-full max-w-[800px] aspect-video bg-effect15White rounded-xl overflow-hidden">
+        <div className="h-full flex flex-col">
+          <div className="flex-grow">
+            <SimliAgent onStart={onStart} onClose={onClose} />
+          </div>
+        </div>
       </div>
     </div>
   );
